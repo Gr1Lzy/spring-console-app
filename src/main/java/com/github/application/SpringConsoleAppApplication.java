1fc79665db70a37ffd60fd9c7eee14299fcdc60b
gr1lzy <kolomoets02@gmail.com>
2024-08-22 00:19:07 +0300

    Update main file

--- src/main/java/com/github/application/SpringConsoleAppApplication.java
+++ src/main/java/com/github/application/SpringConsoleAppApplication.java
@@ -1,13 +1,26 @@
 package com.github.application;
 
+import com.github.application.service.DepartmentService;
+import com.github.application.service.GlobalSearchService;
+import com.github.application.service.LectorService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @RequiredArgsConstructor
-public class SpringConsoleAppApplication {
+public class SpringConsoleAppApplication implements CommandLineRunner {
+    private final LectorService lectorService;
+    private final DepartmentService departmentService;
+    private final GlobalSearchService globalSearchService;
+
     public static void main(String[] args) {
         SpringApplication.run(SpringConsoleAppApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
     }
 }
