600631428cf582aa83fd70bf83365526584843ce
gr1lzy <kolomoets02@gmail.com>
2024-08-21 21:06:21 +0300

    create project

---
+++ src/test/java/org/github/application/SpringConsoleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.github.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConsoleAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
