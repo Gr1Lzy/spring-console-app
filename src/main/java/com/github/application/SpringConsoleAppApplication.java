26e1f4581d5704885b20ca3fa14f3ded63831f11
gr1lzy <kolomoets02@gmail.com>
2024-08-21 23:11:08 +0300

    update pom.xml

--- src/main/java/com/github/application/SpringConsoleAppApplication.java
+++ src/main/java/com/github/application/SpringConsoleAppApplication.java
@@ -1,13 +1,13 @@
-package org.github.application;
+package com.github.application;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RequiredArgsConstructor
 public class SpringConsoleAppApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SpringConsoleAppApplication.class, args);
     }
-
 }
